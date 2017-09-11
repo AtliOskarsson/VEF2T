5178914a547bb31973b1421d1184b7522d9e1ae9
AtliOskarsson <atlios219@nemi.tskoli.is>
2017-09-11 01:27:43 +0000

    Add files via upload

--- Verkefni_3/verkefni_3_1.py
+++ Verkefni_3/verkefni_3_1.py
@@ -16,6 +16,7 @@ def word(word):
                     </head>
                     <body>
                         <a href="bio">Biography</a>
+                        <a href="pictures">Pictures</a>
                         <h1>Steve Jobs</h1>
                         <p>Steve Jobs was born in San Francisco, California, on February 24, 1955, to two University of Wisconsin graduate students who
                         gave him up for adoption. Smart but directionless, Jobs experimented with different pursuits before starting Apple Computer with
@@ -36,14 +37,14 @@ def word(word):
                             <link type="text/css" rel="stylesheet" href="CSS/styles.css">
                     </head>
                     <body>
-                        <a href="bio">Biography</a>
-                        <h1>Biography</h1>
+                        <a href="jobs">Steve Jobs</a>
+                        <a href="pictures">Pictures</a>
+                        <img src="images/biopic.png">
                         <p>Steve Jobs was born in San Francisco, California, on February 24, 1955, to two University of Wisconsin graduate students who
                         gave him up for adoption. Smart but directionless, Jobs experimented with different pursuits before starting Apple Computer with
                         Steve Wozniak in 1976. Apple's revolutionary products, which include the iPod, iPhone and iPad, are now seen as dictating the
                         evolution of modern technology, with Jobs having left the company in 1985 and returning more than a decade later. He died in 2011,
                         following a long battle with pancreatic cancer.</p>
-                        <img src="images/steve_jobs.png">
                     </body>
                 </html>
                 '''
@@ -57,6 +58,7 @@ def word(word):
                             <link type="text/css" rel="stylesheet" href="CSS/styles.css">
                     </head>
                     <body>
+                        <a href="jobs">Steve Jobs</a>
                         <a href="bio">Biography</a>
                         <h1>Pictures</h1>
                         <p>Steve Jobs was born in San Francisco, California, on February 24, 1955, to two University of Wisconsin graduate students who
@@ -72,5 +74,9 @@ def word(word):
 @route('/CSS/<filename>')
 def server_static(filename):
     return static_file(filename, root="CSS")
+
+@route('/images/<filename:re:.*\.png>')
+def server_static(filename):
+    return static_file(filename, root="images", mimetype=".png")
 
 run(host='localhost', port=8080)
